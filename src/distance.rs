38135e2869c05d0a15a0fafde1cccf68c602ec17
BVegNow <117610616+BVegNow@users.noreply.github.com>
2023-02-16 11:30:23 +0800

    feature: add grapheme support

--- src/distance.rs
+++ src/distance.rs
@@ -1,10 +1,12 @@
 use std::cmp;
+extern crate unicode_segmentation;
+use self::unicode_segmentation::UnicodeSegmentation;
 
 fn max_length(str1: &str, str2: &str) -> usize {
     if str1.len() > str2.len() {
-      str1.len()
+        str1.len()
     } else {
-      str2.len()
+        str2.len()
     }
 }
 
@@ -16,7 +18,7 @@ fn get_common_chars(str1: &str, str2: &str, ordering: bool) -> Vec<char> {
     }
 
     let max_length = max_length(str1, str2);
-    let match_buffer = ( max_length / 2 ) - 1;
+    let match_buffer = (max_length / 2) - 1;
     let mut f = str1;
     let mut s = str2;
 
@@ -31,13 +33,13 @@ fn get_common_chars(str1: &str, str2: &str, ordering: bool) -> Vec<char> {
         } else {
             i - match_buffer
         };
-      
-        let e_index: usize = if s.len() <= (i + match_buffer) { 
-            s.len() 
-        } else { 
+
+        let e_index: usize = if s.len() <= (i + match_buffer) {
+            s.len()
+        } else {
             i + match_buffer + 1
         };
-          
+
         let word_slice = &s[s_index..e_index];
         if word_slice.contains(c) {
             common_chars.push(c);
@@ -46,24 +48,22 @@ fn get_common_chars(str1: &str, str2: &str, ordering: bool) -> Vec<char> {
 
     common_chars
 }
-  
+
 pub fn jaro_winkler_distance(str1: &str, str2: &str) -> f32 {
     let matches1 = get_common_chars(str1, str2, false);
     let matches2 = get_common_chars(str1, str2, true);
 
-    if matches1.is_empty() || matches2.is_empty()
-      || matches1.len() != matches2.len()
-    {
-      return 0.0;
+    if matches1.is_empty() || matches2.is_empty() || matches1.len() != matches2.len() {
+        return 0.0;
     }
-    
+
     let mut t = 0.0;
     for (a, b) in matches1.iter().zip(matches2.iter()) {
-      if a != b {
-        t += 0.5;
-      }
+        if a != b {
+            t += 0.5;
+        }
     }
-    
+
     let f1 = matches1.len() as f32 / str1.len() as f32;
     let f2 = matches2.len() as f32 / str2.len() as f32;
     let f3 = (matches1.len() as f32 - t) / matches1.len() as f32;
@@ -72,18 +72,20 @@ pub fn jaro_winkler_distance(str1: &str, str2: &str) -> f32 {
 
 // ported from http://hetland.org/coding/python/levenshtein.py
 pub fn levenshtein_distance(str1: &str, str2: &str) -> usize {
-    let n = str1.len();
-    let m = str2.len();
+    let a_vec: Vec<&str> = UnicodeSegmentation::graphemes(str1, true).collect();
+    let b_vec: Vec<&str> = UnicodeSegmentation::graphemes(str2, true).collect();
 
-    let mut column: Vec<usize> = (0..n + 1).collect();
+    let n = a_vec.len();
+    let m = b_vec.len();
+    // let m = str2.len();
+
+    let mut column: Vec<usize> = (0..=n).collect();
     // TODO this probaly needs to use graphemes
-    let a_vec: Vec<char> = str1.chars().collect();
-    let b_vec: Vec<char> = str2.chars().collect();
-    for i in 1..m + 1 {
+    for i in 1..=m {
         let previous = column;
         column = vec![0; n + 1];
-        column[0] = i;
-        for j in 1..n + 1 {
+        column[0] = 0;
+        for j in 1..=n {
             let add = previous[j] + 1;
             let delete = column[j - 1] + 1;
             let mut change = previous[j - 1];
@@ -96,6 +98,6 @@ pub fn levenshtein_distance(str1: &str, str2: &str) -> usize {
     column[n]
 }
 
-fn min3<T: Ord>(a: T, b: T, c: T) -> T{
+fn min3<T: Ord>(a: T, b: T, c: T) -> T {
     cmp::min(a, cmp::min(b, c))
 }
